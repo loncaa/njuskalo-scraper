8a0c5cb408aab8f7428dc452860ac2fd75c06fe5
= <=>
2016-06-17 23:45:19 +0200

    commit

--- Main.py
+++ Main.py
@@ -10,33 +10,43 @@ dict = dict() #dictionary ili samo {}
 def check_site(sc):
     r = requests.get(link)
     soup = BeautifulSoup(r.content, "html.parser")
-    articles = soup.find_all("article")
+    articles = soup.find_all("article", {"class":"entity-body"})
 
     if articles.__len__() > 0:
-        first_article = articles[0]
-        first_key = first_article.find_all("h3", {"class":"entity-title"})
+        time_to_message = False
 
-        if not first_key[0].text in dict.keys():
+        first_h3_tag = articles[0].find_all("h3", {"class":"entity-title"})
+        first_link = first_h3_tag[0].find_all("a")[0].attrs["href"]
+
+        if not first_link in dict: #.keys
             msg = 'Pojavila se nova karta:\n' #poruka za popup
 
+            links = []
+            prices = []
             for a in articles:
-                h3s = a.find_all("h3", {"class":"entity-title"})
-                prices = a.find_all("strong", {"class":"price price--hrk"})
-                #poveze varijable iz h i p - h[0] - p[0]
-                for h, p in zip(h3s, prices):
-                    if not h.text in dict.keys():
-                        dict[h.text] = p.text
-                        msg += (h.text) + ': ' + (p.text) + '\n'
-                        print((h.text) + ': ' + (p.text))
-                    else:
-                        return
+                h3_tags = a.find_all("h3", {"class":"entity-title"})
+                l = h3_tags[0].find_all("a")
+                p = a.find_all("strong", {"class": "price price--hrk"})
+                if p.__len__() > 0 and l.__len__() > 0:
+                    links.append(l[0].attrs["href"])
+                    prices.append(p[0].text)
 
-            message_box = ctypes.windll.user32.MessageBoxW
-            message_box(None, msg, "Info", 1)
+            for l, p in zip(links, prices):
+                if not l in dict.keys():
+                    time_to_message = True
+                    dict[l] = p
+                    msg += (l) + ': ' + (p) + '\n'
+                    print((l) + ': ' + (p))
+                else:
+                    break
+
+            if time_to_message:
+                message_box = ctypes.windll.user32.MessageBoxW
+                message_box(None, msg, "Info", 1)
         else:
             print("--")
 
-    sc.enter(7*60, 1, check_site, (sc,))
+    sc.enter(2, 1, check_site, (sc,))
 
 check_site(s)
 s.run()
