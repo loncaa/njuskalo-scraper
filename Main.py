25f845ec9a17d3acbb4d0ee7681a47b9ba3ae0dc
= <=>
2016-06-18 17:02:42 +0200

    c

--- Main.py
+++ Main.py
@@ -1,14 +1,20 @@
-import sched, time
-import requests
-import ctypes
+import sched, time #odgadanje zadatka
+import requests #dohvacanje podataka
+import ctypes #popup
+from datetime import datetime #prikazivanje vremena
+from threading import Thread
 from bs4 import BeautifulSoup
 
 s = sched.scheduler(time.time, time.sleep)
-link = "http://www.njuskalo.hr/index.php?ctl=search_ads&keywords=inmusic&sort=new"
+site_http = "http://www.njuskalo.hr/index.php?ctl=search_ads&keywords=inmusic&sort=new"
 dict = dict() #dictionary ili samo {}
 
+def show_message(msg):
+    message_box = ctypes.windll.user32.MessageBoxW
+    message_box(None, msg, "Info", 1)
+
 def check_site(sc):
-    r = requests.get(link)
+    r = requests.get(site_http)
     soup = BeautifulSoup(r.content, "html.parser")
     articles = soup.find_all("article", {"class":"entity-body"})
 
@@ -20,38 +26,38 @@ def check_site(sc):
 
         if not first_link in dict: #.keys
             msg = 'Pojavila se nova karta:\n' #poruka za popup
-
-            links = [] #linkovi su junik key za artikl
-            prices = [] #cijena artikla
-            titles = [] #naziv artikla
             for a in articles:
+                time_to_break = False
                 h3_tags = a.find_all("h3", {"class":"entity-title"})
-                l = h3_tags[0].find_all("a")
-                p = a.find_all("strong", {"class": "price price--hrk"})
+                link = h3_tags[0].find_all("a")
+                price = a.find_all("strong", {"class": "price price--hrk"})
 
                 #spremanje podataka u niz
-                if p.__len__() > 0 and l.__len__() > 0:
-                    links.append(l[0].attrs["href"])
-                    prices.append(p[0].text)
-                    titles.append(l[0].text)
+                if price.__len__() > 0 and link.__len__() > 0:
+                    l = link[0].attrs["href"] #link
+                    p = price[0].text #price
+                    t = link[0].text #title
 
-            #nizovi su spojeni po indexu
-            for l, p, t in zip(links, prices, titles):
-                if not l in dict.keys():
-                    time_to_message = True
-                    dict[l] = p
-                    msg += (t) + ': ' + (p) + '\n'
-                    print((t) + ': ' + (p))
-                else:
-                    break
+                    if not l in dict.keys():
+                        time_to_message = True
+                        dict[l] = p
+                        msg += t + ': ' + p + '\n'
+                        print("++ New ticke: " + t + ': ' + p + "\n\t++link: " + l)
+                    else:
+                        time_to_break = True
+                        break
+                #if time_to_break is True:
+                #   break
 
             if time_to_message:
-                message_box = ctypes.windll.user32.MessageBoxW
-                message_box(None, msg, "Info", 1)
+                thread = Thread(target=show_message, args=(msg,))
+                thread.start()
+                #DODATI ZVUK
+                #thread.join() #čeka nit da zavrsi s radom
         else:
-            print("--")
+            print("-- Not new tickets at " + datetime.now().strftime("%Y-%m-%d in %H:%M:%S") + "--")
 
-    sc.enter(5*60, 1, check_site, (sc,))
+    sc.enter(3*60, 1, check_site, (sc,))
 
 check_site(s)
 s.run()
